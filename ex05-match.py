2c589e104eabd62aca2ad72551f064b25bcb0e25
Rafaella Matos <163598367+profrafamatos@users.noreply.github.com>
2026-02-25 13:44:18 +0000

    Atualização do match

--- ex05-match.py
+++ ex05-match.py
@@ -1,10 +1,11 @@
 dia_da_semana = int(input("Informe um dia da semana entre 1 e 7: "))
+mes = int(input("Informe o mês do ano: "))
 
 match dia_da_semana:
-    case 1 | 2 | 3 | 4 | 5:
-        print("Dia de semana")
-    case 6 | 7:
-        print("Fim de semana")
+    case 1 | 2 | 3 | 4 | 5 if mes == 4:
+        print("Dia de semana do mês de Abril")
+    case 6 | 7 if mes == 2:
+        print("Fim de semana do mês de Fevereiro")
     case _:
         print("Dia inválido")
         
